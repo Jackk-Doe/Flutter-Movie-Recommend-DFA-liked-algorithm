93dee30e5b39885b340d25f03a2e958cb7a240d6
Jackk-Doe <dodo-stv@hotmail.com>
2022-10-21 16:42:56 +1300

    Update : HomePage UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  void _startGetRecommendMovie() {
+    // TODO : Go to Pick Movie type page
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("This is Home Page"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Home Page', style: TextStyle(fontSize: 44)),
+            const SizedBox(height: 30),
+
+            CustomElevatedButton(onPressfunc: _startGetRecommendMovie, buttonText: "START")
+          ],
+        ),
       ),
     );
   }
