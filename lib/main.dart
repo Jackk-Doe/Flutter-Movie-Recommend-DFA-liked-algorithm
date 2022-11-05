e7db623fb3c74a8d4a67b8ba5af5908ce6646a65
Jackk-Doe <dodo-stv@hotmail.com>
2022-11-05 22:19:32 +1300

    Update : main.dart, to enable App Router used

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:movie_recommend_dfa/navigation/app_router.dart';
+import 'package:movie_recommend_dfa/providers/providers.dart';
 
 import 'pages/pages.dart';
 
@@ -6,17 +10,50 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  /*
+  Providers 
+  */
+  final _appStateProvider = AppStateProvider();
+
+  /*
+  Navigator 2.0 : App router
+  */
+  late AppRouter _appRouter;
+
+  @override
+  void initState() {
+    _appRouter = AppRouter(
+      appStateProvider: _appStateProvider,
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => _appStateProvider,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Router(
+          routerDelegate: _appRouter,
+          backButtonDispatcher: RootBackButtonDispatcher(),
+        ),
       ),
-      home: const HomePage(),
     );
   }
 }
