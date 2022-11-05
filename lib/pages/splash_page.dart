1a77befe9d5a3a6bcdbb1f31b6b855a074ea95ac
Jackk-Doe <dodo-stv@hotmail.com>
2022-11-05 22:16:22 +1300

    Create : Splash page, basic

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:movie_recommend_dfa/providers/providers.dart';
+
+import 'package:provider/provider.dart';
+
+class SplashPage extends StatefulWidget {
+
+  static MaterialPage page() {
+    return const MaterialPage(child: SplashPage());
+  }
+
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Provider.of<AppStateProvider>(context, listen: false).testInternetConnection();
+    Provider.of<AppStateProvider>(context, listen: false).testApiKey();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "Starting the app",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w400
+              ),
+            ),
+            SizedBox(height: 20),
+
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
