a2e20a6b51bc7dbd482b839f6d74ae5f98f04a66
Jackk-Doe <dodo-stv@hotmail.com>
2022-11-06 23:53:11 +1300

    Create : Movie-Genre data model file

---
+++ lib/models/movie_genre.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+class MovieGenre {
+  final String name;
+  final int id;
+
+  MovieGenre({
+    required this.name,
+    required this.id,
+  });
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+  
+    result.addAll({'name': name});
+    result.addAll({'id': id});
+  
+    return result;
+  }
+
+  factory MovieGenre.fromMap(Map<String, dynamic> map) {
+    return MovieGenre(
+      name: map['name'] ?? '',
+      id: map['id']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MovieGenre.fromJson(String source) => MovieGenre.fromMap(json.decode(source));
+}
