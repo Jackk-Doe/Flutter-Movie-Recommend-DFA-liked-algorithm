315c4e2e347c7e7822e098257040e8ed19d441e6
Jackk-Doe <dodo-stv@hotmail.com>
2022-11-06 23:51:56 +1300

    Create : API call function to get Genre List

--- lib/services/tmdb_api_services.dart
+++ lib/services/tmdb_api_services.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
+import '../models/models.dart';
 import '../constants/constants.dart';
 
 class TmdbApiServices {
@@ -22,5 +25,32 @@ class TmdbApiServices {
     } catch (e) {
       return false;
     }
+  }
+
+
+  /// Use this to get List of Genre
+  static Future<List<MovieGenre>> getGenres() async {
+    List<MovieGenre> genres = [];
+
+    try {
+      http.Response res = await http.get(
+        Uri.parse(
+          "${Constants.BASE_URL}/genre/movie/list?${Constants.API_KEY_QUERY}"
+        ),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+      );
+
+      if (res.statusCode == 200) {
+        Map<String, dynamic> decoded = jsonDecode(res.body);
+        List<dynamic> genresMap = decoded["genres"];
+        List<MovieGenre> genreModels = genresMap.map((e) => MovieGenre.fromMap(e)).toList();
+        genres = genreModels;
+      }
+    } catch (e) {
+      // NOTE : Not sure what to catch here
+    }
+    return genres;
   }
 }
