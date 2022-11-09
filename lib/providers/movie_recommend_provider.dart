9f7d91e60800f7301af1b7eb175accb62911d599
Jackk-Doe <dodo-stv@hotmail.com>
2022-11-09 20:38:40 +1300

    Create : provider: var & func of selected MovieIDs
    - In Movie-Recom provider
    - create [_recordedMovieIds] to record selected Movie.Ids
    - also some helpful funtions

--- lib/providers/movie_recommend_provider.dart
+++ lib/providers/movie_recommend_provider.dart
@@ -9,10 +9,26 @@ class MovieRecommendProvider extends ChangeNotifier {
   /// [_interested] <GenreID, count> : records user's interested genre
   Map<int, int> _interested = {};
 
-  Map<int, int> get interestedGenre => _interested;
+  /// To record selected movie id into a list
+  Set<int> _recordedMovieIds = {};
 
-  void recordInterestGenre(MovieGenre interestGenre) {
-    int currentCount = _interested.putIfAbsent(interestGenre.id, () => 0);
-    _interested[interestGenre.id] = currentCount + 1;
+
+  Map<int, int> get interestedGenre => _interested;
+  List<int> get recordedMovieIds => _recordedMovieIds.toList(growable: false);
+
+  /// Record selected genreId into [_interested] map value
+  void recordInterestGenreId(int genreId) {
+    int currentCount = _interested.putIfAbsent(genreId, () => 0);
+    _interested[genreId] = currentCount + 1;
+  }
+
+  /// Record selected Movie's genre.ids into [_interested] map value
+  void recordInterestGenreIdList(List<int> idList) {
+    idList.forEach(recordInterestGenreId);
+  }
+
+  /// Record selected Movie.id into [_recordedMovieIds] map value
+  void recordSelectedMovieId(int movieId) {
+    _recordedMovieIds.add(movieId);
   }
 }
